208262837c8a7394149f94858c47c98bbf07f5a9
sskullgra <marlon@urban.com.au>
2020-03-15 21:40:53 +0800

    Updates

---
+++ precache-manifest.ea57ae640efa087679ef19603d6cf0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d477fab429a5705aa06e23562c36c7d3",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "6fac9562d8e17d06bda4",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88a1e37d73aa94365f52",
+    "url": "/hello/static/js/2.5c34b3f1.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/hello/static/js/2.5c34b3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fac9562d8e17d06bda4",
+    "url": "/hello/static/js/main.13ce8b39.chunk.js"
+  },
+  {
+    "revision": "9a997dcc3847ef98a7fa",
+    "url": "/hello/static/js/runtime-main.0becb6b6.js"
+  }
+]);
